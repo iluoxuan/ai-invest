685c61c00a5e63f6161256e5c6f39925f62dd9f1
ljq <lidaxia2022@gmail.com>
2024-12-09 19:02:32 +0800

    搜索添加页面

--- ai-invest-uniapp/unpackage/dist/dev/mp-weixin/app.js
+++ ai-invest-uniapp/unpackage/dist/dev/mp-weixin/app.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
 const common_vendor = require("./common/vendor.js");
+const config_env = require("./config/env.js");
 if (!Math) {
   "./pages/user/welcome/step1.js";
   "./pages/user/welcome/step2.js";
@@ -9,19 +10,55 @@ if (!Math) {
   "./pages/user/init/home.js";
   "./pages/start/stepOne.js";
   "./pages/start/stepTwo.js";
-  "./pages/stock/add.js";
+  "./pages/stock/searchAdd.js";
   "./pages/stock/addPlan.js";
 }
 const _sfc_main = {
   __name: "App",
   setup(__props) {
-    common_vendor.onLaunch(() => {
-      const buttonBound = common_vendor.index.getStorageInfoSync("buttonBound");
-      if (!buttonBound) {
-        const res = common_vendor.index.getMenuButtonBoundingClientRect();
-        console.log(res);
-        common_vendor.index.setStorageSync("buttonBound", res);
+    const userInfoUrl = `${config_env.env.INVEST_URL}/api/user/info`;
+    function storeButtonBound() {
+      try {
+        const buttonBound = common_vendor.index.getStorageSync("buttonBound");
+        if (!buttonBound) {
+          const res = common_vendor.index.getMenuButtonBoundingClientRect();
+          console.log(res);
+          common_vendor.index.setStorageSync("buttonBound", JSON.stringify(res));
+        }
+      } catch (error) {
+        console.error("获取胶囊按钮位置失败:", error);
       }
+    }
+    async function checkUserAndNavigate() {
+      try {
+        const [error, res] = await common_vendor.index.request({
+          url: userInfoUrl,
+          method: "POST"
+        });
+        if (error || res.statusCode !== 200 || !res.data.success) {
+          console.error("获取用户信息失败:", res || error);
+          common_vendor.index.reLaunch({
+            url: "/pages/user/init/home"
+          });
+          return;
+        }
+        const userInfo = res.data;
+        common_vendor.index.setStorageSync("userInfo", JSON.stringify(userInfo));
+        const shouldShowGuide = userInfo.hadInitAccount;
+        const targetPage = shouldShowGuide ? "/pages/user/init/home" : "/pages/user/welcome/step1";
+        common_vendor.index.reLaunch({
+          url: targetPage
+        });
+      } catch (error) {
+        console.error("请求失败:", error);
+        common_vendor.index.reLaunch({
+          url: "/pages/user/init/home"
+        });
+      }
+    }
+    common_vendor.onLaunch(async () => {
+      storeButtonBound();
+      await checkUserAndNavigate();
     });
     return () => {
     };
--- ai-invest-uniapp/unpackage/dist/dev/mp-weixin/pages/stock/searchAdd.js
+++ ai-invest-uniapp/unpackage/dist/dev/mp-weixin/pages/stock/searchAdd.js
@@ -1,17 +1,15 @@
 "use strict";
-const common_vendor = require("../../../common/vendor.js");
-const config_env = require("../../../config/env.js");
-const url = `${config_env.env.INVEST_URL}/api/account/info`;
+const common_vendor = require("../../common/vendor.js");
+const config_env = require("../../config/env.js");
+const url = `${config_env.env.INVEST_URL}/api/stock/search`;
 console.log("url", url);
 const _sfc_main = {
   data() {
     return {
-      account: {},
+      searchQuery: "",
+      // 用于存储输入框的内容
       holdings: []
     };
-  },
-  onLoad() {
-    this.getAccountInfo();
   },
   methods: {
     // 定义一个包装函数，返回 Promise
@@ -24,61 +22,53 @@ const _sfc_main = {
         });
       });
     },
-    async getAccountInfo() {
+    async searchBuy() {
       var _a, _b;
       try {
+        let query = this.searchQuery;
+        console.log("用户输入的查询:", query);
         const [err, res] = await this.requestWrapper({
           url,
           method: "POST",
           header: {
             "content-type": "application/json"
           },
-          data: {},
+          data: { "keyWord": query },
           complete: (res2) => {
             console.log("请求完成:", res2);
           }
         });
         if (err) {
-          console.error("获取账户信息失败:", err);
-          throw new Error("获取账户信息失败");
+          console.error("搜索加仓:", err);
+          throw new Error("搜索股票失败");
         }
         if (!res.data || !res.data.success) {
           console.error("API 调用不成功:", ((_a = res.data) == null ? void 0 : _a.msg) || "未知错误");
           throw new Error(((_b = res.data) == null ? void 0 : _b.msg) || "未知错误");
         }
-        this.account = res.data.data.account;
-        this.holdings = res.data.data.stockList;
+        this.holdings = res.data.data;
       } catch (error) {
-        console.error("投资账户初始化发生错误:", error.message);
+        console.error("搜索股票失败:", error.message);
         throw error;
       }
     },
-    addStock() {
+    addPlan(holding, index) {
       this.navigateToNextPage();
     },
     navigateToNextPage() {
       common_vendor.index.navigateTo({
-        url: "/pages/stock/searchAdd"
-        // 替换为目标页面的路径
-      });
-    },
-    buyPlan(holding, index) {
-      console.log("buyPlan", JSON.stringify(holding), index);
-      common_vendor.index.navigateTo({
         url: "/pages/stock/addPlan"
+        // 替换为目标页面的路径
       });
     }
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: common_vendor.t($data.account.totalAmount),
-    b: common_vendor.t($data.account.monthlyExpenses),
-    c: common_vendor.t($data.account.monthlyIncome),
-    d: common_vendor.t($data.account.planAmount),
-    e: common_vendor.t($data.account.availableAmount),
-    f: common_vendor.t($data.account.inverstDay),
-    g: common_vendor.f($data.holdings, (holding, index, i0) => {
+    a: common_vendor.o((...args) => $options.searchBuy && $options.searchBuy(...args)),
+    b: $data.searchQuery,
+    c: common_vendor.o(($event) => $data.searchQuery = $event.detail.value),
+    d: common_vendor.f($data.holdings, (holding, index, i0) => {
       return {
         a: common_vendor.t(holding.stockCnName),
         b: common_vendor.t(holding.pe),
@@ -86,13 +76,10 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
         d: common_vendor.t(holding.holdings),
         e: common_vendor.t(holding.price),
         f: index,
-        g: common_vendor.o(($event) => $options.buyPlan(holding, index), index)
+        g: common_vendor.o(($event) => $options.addPlan(holding, index), index)
       };
-    }),
-    h: common_vendor.o(($event) => $options.addStock("加仓")),
-    i: common_vendor.o(($event) => _ctx.market("市场")),
-    j: common_vendor.o(($event) => _ctx.subStock("市场"))
+    })
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-8f6ab2a6"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-2db44495"]]);
 wx.createPage(MiniProgramPage);
