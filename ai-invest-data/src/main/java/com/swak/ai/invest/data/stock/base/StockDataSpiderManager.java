bd9c87d3a8e94de68ca66eafcb218c168c87f921
ljq <lidaxia2022@gmail.com>
2024-12-15 00:56:30 +0800

    抓取雪球港股数据1

--- ai-invest-data/src/main/java/com/swak/ai/invest/data/stock/base/StockDataSpiderManager.java
+++ ai-invest-data/src/main/java/com/swak/ai/invest/data/stock/base/StockDataSpiderManager.java
@@ -2,8 +2,10 @@ package com.swak.ai.invest.data.stock.base;
 
 import com.alicp.jetcache.anno.CacheType;
 import com.alicp.jetcache.anno.Cached;
+import com.swak.ai.invest.common.entity.MarketEnum;
 import com.swak.ai.invest.common.entity.stock.StockQuote;
 import com.swak.ai.invest.common.exception.SpiderDataException;
+import com.swak.tushar.entity.basic.Stock;
 import com.swak.tushar.entity.trade.StockDailyBasic;
 import com.swak.tushar.entity.trade.StockTradeLine;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,11 @@ public class StockDataSpiderManager implements StockDataSpider {
     private final List<StockDataSpider> stockDataSpiders;
 
 
+    @Override
+    public List<Stock> basic(MarketEnum market) {
+        return callSpider(spider -> spider.basic(market));
+    }
+
     /**
      * 实时数据，加本地缓存
      */
@@ -32,25 +39,25 @@ public class StockDataSpiderManager implements StockDataSpider {
     @Override
     public StockQuote quote(String tsCode) {
 
-        return callSpider(tsCode, spider -> spider.quote(tsCode));
+        return callSpider(spider -> spider.quote(tsCode));
     }
 
     @Override
     public List<StockTradeLine> dailyLine(String tsCode) {
-        return callSpider(tsCode, spider -> spider.dailyLine(tsCode));
+        return callSpider(spider -> spider.dailyLine(tsCode));
     }
 
     @Override
     public StockDailyBasic dailyBasic(String tsCode) {
-        return callSpider(tsCode, spider -> spider.dailyBasic(tsCode));
+        return callSpider(spider -> spider.dailyBasic(tsCode));
     }
 
-    private <T> T callSpider(String tsCode, Function<StockDataSpider, T> spiderFunction) {
+    private <T> T callSpider(Function<StockDataSpider, T> spiderFunction) {
         for (StockDataSpider spider : stockDataSpiders) {
             try {
                 return spiderFunction.apply(spider);
             } catch (Exception e) {
-                log.error("spider tsCode={} failed", tsCode, e);
+                log.error("spider failed", e);
             }
         }
         throw new SpiderDataException("抓取数据失败");
