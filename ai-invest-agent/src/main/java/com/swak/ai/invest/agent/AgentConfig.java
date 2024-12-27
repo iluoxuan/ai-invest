e955ed82c6bcde353ed2702e84a34d5095673813
ljq <iluoxuan@163.com>
2024-12-27 11:25:06 +0800

    新增-langchain4j-本地ollama部署qwen:7b

---
+++ ai-invest-agent/src/main/java/com/swak/ai/invest/agent/AgentConfig.java
@@ -0,0 +1,12 @@
+package com.swak.ai.invest.agent;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: junqing.li
+ * @date: 2024/12/27 11:19
+ */
+@Configuration
+public class AgentConfig {
+
+}
